d54d725927430e80fbcb2ac2a72c228071bc84a9
tonnymuchui <tonykanyingah@gmail.com>
2026-03-19 02:25:57 +0300

    feat: add change password request DTO

---
+++ src/main/java/com/picknquicks/dto/request/ChangePasswordRequest.java
@@ -0,0 +1,26 @@
+package com.picknquicks.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChangePasswordRequest {
+
+    @NotBlank(message = "Current password is required")
+    private String currentPassword;
+
+    @NotBlank(message = "New password is required")
+    @Size(min = 8, max = 255, message = "New password must be between 8 and 255 characters")
+    private String newPassword;
+
+    @NotBlank(message = "Confirm password is required")
+    private String confirmPassword;
+}
+
