a0c9960bb6425cc86bd9d90813aaf7b8bba4b6ef
tonnymuchui <tonykanyingah@gmail.com>
2026-03-19 02:27:58 +0300

    refactor: enhance user principal with additional security context

--- src/main/java/com/picknquicks/security/UserPrincipal.java
+++ src/main/java/com/picknquicks/security/UserPrincipal.java
@@ -19,7 +19,7 @@ public class UserPrincipal implements UserDetails {
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return user.getRoles().stream()
-                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
                 .collect(Collectors.toList());
     }
 
