92dfe566e87742327a6f7d5c6b2b3c29d696c6d5
tonnymuchui <tonykanyingah@gmail.com>
2026-05-03 23:19:09 +0300

    feat: Add CartEventListener

--- src/main/java/com/picknquicks/event/listener/CartEventListener.java
+++ src/main/java/com/picknquicks/event/listener/CartEventListener.java
@@ -1,4 +1,47 @@
 package com.picknquicks.event.listener;
+import com.picknquicks.event.CartAbandonedEvent;
+import com.picknquicks.event.CartItemAddedEvent;
+import com.picknquicks.event.CartMergedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
+@Slf4j
 public class CartEventListener {
+
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Async
+    @EventListener
+    public void handleCartItemAdded(CartItemAddedEvent event) {
+        log.info("Cart item added event: cartId={}, productId={}, quantity={}",
+                event.getCartId(), event.getProductId(), event.getQuantity());
+
+        kafkaTemplate.send("cart-item-added", event.getCartId().toString(), event);
+    }
+
+    @Async
+    @EventListener
+    public void handleCartAbandoned(CartAbandonedEvent event) {
+        log.info("Cart abandoned event: cartId={}, email={}, itemCount={}",
+                event.getCartId(), event.getEmail(), event.getItemCount());
+
+        kafkaTemplate.send("cart-abandoned", event.getCartId().toString(), event);
+
+        log.info("Abandoned cart email queued for {}", event.getEmail());
+    }
+
+    @Async
+    @EventListener
+    public void handleCartMerged(CartMergedEvent event) {
+        log.info("Cart merged event: guestCartId={}, userCartId={}",
+                event.getGuestCartId(), event.getUserCartId());
+
+        kafkaTemplate.send("cart-merged", event.getUserCartId().toString(), event);
+    }
 }
