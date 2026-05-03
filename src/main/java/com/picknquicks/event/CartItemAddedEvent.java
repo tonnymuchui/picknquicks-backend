ebfe7bda17788d3918f40b93355314ca46968503
tonnymuchui <tonykanyingah@gmail.com>
2026-05-03 23:18:51 +0300

    feat: Add CartItemAddedEvent

--- src/main/java/com/picknquicks/event/CartItemAddedEvent.java
+++ src/main/java/com/picknquicks/event/CartItemAddedEvent.java
@@ -1,4 +1,22 @@
 package com.picknquicks.event;
+import java.util.UUID;
 
-public class CartItemAddedEvent {
+public class CartItemAddedEvent extends CartEvent {
+
+    private final UUID productId;
+    private final Integer quantity;
+
+    public CartItemAddedEvent(Object source, UUID cartId, UUID productId, Integer quantity) {
+        super(source, cartId, "CART_ITEM_ADDED");
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
+    public UUID getProductId() {
+        return productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
 }
