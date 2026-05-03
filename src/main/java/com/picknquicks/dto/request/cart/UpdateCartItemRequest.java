db09dfdb5701d90f93d5d0a257c8333e05137eda
tonnymuchui <tonykanyingah@gmail.com>
2026-05-03 23:18:11 +0300

    feat: Add UpdateCartItemRequest DTO

--- src/main/java/com/picknquicks/dto/request/cart/UpdateCartItemRequest.java
+++ src/main/java/com/picknquicks/dto/request/cart/UpdateCartItemRequest.java
@@ -1,4 +1,21 @@
 package com.picknquicks.dto.request.cart;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(description = "Update cart item request")
 public class UpdateCartItemRequest {
+
+    @Schema(description = "New quantity", example = "2")
+    @NotNull(message = "Quantity is required")
+    @Min(value = 1, message = "Quantity must be at least 1")
+    private Integer quantity;
 }
