4fe3b1fbbf880f6ffb168b25ebef85742040d54e
tonnymuchui <tonykanyingah@gmail.com>
2026-05-03 23:19:51 +0300

    feat: Add CartService interface

--- src/main/java/com/picknquicks/service/cart/CartService.java
+++ src/main/java/com/picknquicks/service/cart/CartService.java
@@ -1,4 +1,24 @@
 package com.picknquicks.service.cart;
+import com.picknquicks.dto.request.cart.AddToCartRequest;
+import com.picknquicks.dto.request.cart.UpdateCartItemRequest;
+import com.picknquicks.dto.response.cart.CartResponse;
+import java.util.UUID;
 
-public class CartService {
+public interface CartService {
+
+    CartResponse addToCart(AddToCartRequest request, String guestToken);
+
+    CartResponse updateCartItem(UUID cartItemId, UpdateCartItemRequest request, String guestToken);
+
+    CartResponse removeFromCart(UUID cartItemId, String guestToken);
+
+    CartResponse clearCart(String guestToken);
+
+    CartResponse getCart(String guestToken);
+
+    CartResponse mergeGuestCart(String guestToken, UUID userId);
+
+    void markAsAbandoned(UUID cartId);
+
+    void cleanupExpiredCarts();
 }
